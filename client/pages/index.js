33265f7f7a5c38c93fd3f100c673c7e6830e7fa9
Dustin Sinkey <dustin@cherre.com>
2022-09-14 07:33:18 -0300

    Add custom hook, add route to get user for server side rendering

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,31 @@
-const Index = () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log('I am in the component', currentUser);
   return <h1>Landing Page</h1>;
 };
 
-export default Index;
+LandingPage.getInitialProps = async ({ req }) => {
+  console.log(req.headers);
+  if (typeof window === 'undefined') {
+    const url =
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser';
+
+    const { data } = await axios.get(url, {
+      headers: req.headers,
+    });
+
+    console.log({ data });
+
+    return data;
+  } else {
+    const { data } = await axios.get('/api/users/currentuser');
+    console.log('data', data);
+
+    return data;
+  }
+
+  return {};
+};
+
+export default LandingPage;
