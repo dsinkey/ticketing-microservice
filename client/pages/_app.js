0b5c032054c72d73266b36b226dc42fbf4601961
Dustin Sinkey <dustin@cherre.com>
2022-09-14 09:25:36 -0300

    Add signin form and allow getInitialProps to be called in AppComponent and other components that need it as well

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <h1>Current User</h1>
+      <Component {...pageProps} currentUser={currentUser} />
+    </div>
+  );
 };
 
-export default App;
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,15 @@
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  console.log('I am in the component', currentUser);
-  return <h1>Landing Page</h1>;
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  );
 };
 
 LandingPage.getInitialProps = async (context) => {
+  console.log('LANDING PAGE');
   const { data } = await buildClient(context).get('/api/users/currentuser');
   return data;
 };
