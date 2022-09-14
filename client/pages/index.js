20ec49b1205e23739a8a8b34764719ad2e959a16
Dustin Sinkey <dustin@cherre.com>
2022-09-14 08:35:37 -0300

    Clean up deployments and add client for making api call on both client and server

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,31 +1,13 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   console.log('I am in the component', currentUser);
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  console.log(req.headers);
-  if (typeof window === 'undefined') {
-    const url =
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser';
-
-    const { data } = await axios.get(url, {
-      headers: req.headers,
-    });
-
-    console.log({ data });
-
-    return data;
-  } else {
-    const { data } = await axios.get('/api/users/currentuser');
-    console.log('data', data);
-
-    return data;
-  }
-
-  return {};
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get('/api/users/currentuser');
+  return data;
 };
 
 export default LandingPage;
