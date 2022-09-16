bbd5189c92d66fb96ff4519430337b081b6f8518
Dustin Sinkey <dustin@cherre.com>
2022-09-16 13:31:26 -0300

    Add tests and routes for new, show, index

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,8 +2,10 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler } from '@ds-tickets/common';
-import { NotFoundError } from '@ds-tickets/common';
+import { errorHandler, NotFoundError, currentUser } from '@ds-tickets/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketsRouter } from './routes';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,6 +16,11 @@ app.use(
     secure: process.env.NODE_ENV !== 'test',
   })
 );
+
+app.use(currentUser);
+app.use(showTicketRouter);
+app.use(createTicketRouter);
+app.use(indexTicketsRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,9 +1,13 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
+import jwt from 'jsonwebtoken';
+
+declare global {
+  var signin: () => string[];
+}
 
 let mongo: any;
-
 beforeAll(async () => {
   process.env.JWT_KEY = 'asdfasdf';
   mongo = await MongoMemoryServer.create();
@@ -26,3 +30,15 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+global.signin = () => {
+  const payload = {
+    id: 'fhnduyf8w35oasdhf',
+    email: 'test@test.com',
+  };
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  const session = { jwt: token };
+  const sessionJSON = JSON.stringify(session);
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+  return [`session=${base64}`];
+};
